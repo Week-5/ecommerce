954e2d89009bf931a9d54d43521617c49c9bc02f
Danny Reina <dannyreina@google.com>
2021-12-03 17:56:12 +0000

    fix: update routes for User.js file

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const {db} = require('./db');
-const {Admin} = require('./classes/Admin');
+const {User} = require('./classes/User');
 const {Customer} = require('./classes/Customer');
 const {Item} = require('./classes/Item');
 const {Cart} = require('./classes/Cart');
 
-Admin.hasOne(Cart);
-Cart.belongsTo(Admin);
+User.hasOne(Cart);
+Cart.belongsTo(User);
 
-Admin.hasMany(Item);
-Item.belongsTo(Admin);
+User.hasMany(Item);
+Item.belongsTo(User);
 
 Customer.hasOne(Cart);
 Cart.belongsTo(Customer);
@@ -18,4 +18,4 @@ Item.belongsTo(Cart);
 // reference for deleting profile cascade
 // User.hasMany(Restaurant, {onDelete: 'cascade'});
 
-module.exports = {Admin, Customer, Item, Cart};
+module.exports = {User, Customer, Item, Cart};
