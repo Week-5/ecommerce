c016366abf565d5ec4937c68ca09a5e332bbd97c
Danny Reina <dannyreina@google.com>
2021-12-03 19:42:15 +0000

    fix: relationships

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const {db} = require('./db');
 const {User} = require('./classes/User');
-const {Customer} = require('./classes/Customer');
 const {Item} = require('./classes/Item');
 const {Cart} = require('./classes/Cart');
 
@@ -10,12 +9,10 @@ Cart.belongsTo(User);
 User.hasMany(Item);
 Item.belongsTo(User);
 
-Customer.hasOne(Cart);
-Cart.belongsTo(Customer);
 
 Cart.hasMany(Item);
 Item.belongsTo(Cart);
 // reference for deleting profile cascade
 // User.hasMany(Restaurant, {onDelete: 'cascade'});
 
-module.exports = {User, Customer, Item, Cart};
+module.exports = {User, Item, Cart};
