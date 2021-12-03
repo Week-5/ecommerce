4526f9b763d69582058ecca1f9c230c968135c66
Eric Davenport <edavenportjr@google.com>
2021-12-03 22:46:16 +0000

    Feat: Test, Cart, Item, User.test only  pass

---
+++ test/Cart.test.js
@@ -0,0 +1,6 @@
+const { Cart } = require('../index')
+
+describe('Cart Object Test', () => {
+    // TODO: No Cart test created yet
+    // Once cart functionality is address -> will be able to maneuver the cart object
+});
