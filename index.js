b00098afb7a9c54da588943c5688c6ce495f4422
Danny Reina <dannyreina@google.com>
2021-12-03 21:24:28 +0000

    feat: enablle cascading removal of resourrces

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const {User} = require('./classes/User');
 const {Item} = require('./classes/Item');
 const {Cart} = require('./classes/Cart');
 
-User.hasOne(Cart);
-Cart.belongsTo(User);
+User.hasOne(Cart, {onDelete: 'cascade'});
+Cart.belongsTo(User, {onDelete: 'cascade'});
 
-User.hasMany(Item);
-Item.belongsTo(User);
+User.hasMany(Item, {onDelete: 'cascade'});
+Item.belongsTo(User, {onDelete: 'cascade'});
 
 
-Cart.hasMany(Item);
-Item.belongsTo(Cart);
+Cart.hasMany(Item, {onDelete: 'cascade'});
+Item.belongsTo(Cart, {onDelete: 'cascade'});
 // reference for deleting profile cascade
 // User.hasMany(Restaurant, {onDelete: 'cascade'});
 
