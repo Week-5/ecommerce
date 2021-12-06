8c432e0f21cb67eb26c554ae7c6beb521d1d5cfa
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2021-12-06 15:23:07 -0700

    Update Item.js

--- classes/Item.js
+++ classes/Item.js
@@ -1,25 +1,25 @@
-const {db, DataTypes, Model} = require('../db');
+const { db, DataTypes, Model } = require('../db');
 /**
  * Item class extends Model
  */
 class Item extends Model {}
 
 Item.init(
-    {
-      title: DataTypes.STRING,
-      stock: DataTypes.INTEGER,
-      price: DataTypes.FLOAT,
-      description: DataTypes.STRING,
-      category: DataTypes.STRING,
-      image: DataTypes.STRING,
-      clickCount: DataTypes.INTEGER,
-    },
-    {
-      sequelize: db,
-      timestamps: false,
-    },
+	{
+		title: DataTypes.STRING,
+		stock: DataTypes.INTEGER,
+		price: DataTypes.FLOAT,
+		description: DataTypes.STRING,
+		category: DataTypes.STRING,
+		image: DataTypes.STRING,
+		clickCount: DataTypes.INTEGER,
+	},
+	{
+		sequelize: db,
+		timestamps: false,
+	}
 );
 
 module.exports = {
-  Item,
+	Item,
 };
