b9b9f996881c1e80a29c742453eb9ff4c50398f4
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2021-12-06 15:23:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const {db} = require('./db');
-const {User} = require('./classes/User');
-const {Item} = require('./classes/Item');
-const {Cart} = require('./classes/Cart');
+const { db } = require('./db');
+const { User } = require('./classes/User');
+const { Item } = require('./classes/Item');
+const { Cart } = require('./classes/Cart');
 
-User.hasOne(Cart, {onDelete: 'cascade'});
-Cart.belongsTo(User, {onDelete: 'cascade'});
+User.hasOne(Cart, { onDelete: 'cascade' });
+Cart.belongsTo(User, { onDelete: 'cascade' });
 
-User.hasMany(Item, {onDelete: 'cascade'});
-Item.belongsTo(User, {onDelete: 'cascade'});
+User.hasMany(Item, { onDelete: 'cascade' });
+Item.belongsTo(User, { onDelete: 'cascade' });
 
-
-Cart.hasMany(Item, {onDelete: 'cascade'});
-Item.belongsTo(Cart, {onDelete: 'cascade'});
+Cart.hasMany(Item, { onDelete: 'cascade' });
+Item.belongsTo(Cart, { onDelete: 'cascade' });
 // reference for deleting profile cascade
 // User.hasMany(Restaurant, {onDelete: 'cascade'});
 
-module.exports = {User, Item, Cart};
+module.exports = { User, Item, Cart };
