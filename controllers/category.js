60354a9a252928935e29b434dadf32b942685d0e
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2021-12-06 21:25:48 -0700

    Create category.js

---
+++ controllers/category.js
@@ -0,0 +1,41 @@
+const { User, Item, Cart } = require('../index');
+
+////////////////////////////////
+//      JEWELRY CATEGORY      //
+///////////////////////////////
+exports.getJewelry = async (req, res) => {
+  const jewelry = await Item.findAll({ where: { category: 'jewelry' } });
+  res.render('category', { jewelry });
+};
+
+////////////////////////////////
+//      ELECTRONICS CATEGORY      //
+///////////////////////////////
+exports.getElectronics = async (req, res) => {
+  console.log(req.body);
+  const electronics = await Item.findAll({
+    where: { category: 'Electronics' },
+  });
+  res.render('category', { electronics });
+};
+
+////////////////////////////////
+//  MEN'S CLOTHING CATEGORY   //
+///////////////////////////////
+exports.getMensClothing = async (req, res) => {
+  const maleClothing = await Item.findAll({
+    where: { category: "Men's Clothing" },
+  });
+  res.render('category', { maleClothing });
+};
+
+////////////////////////////////
+//      WOMEN'S CATEGORY      //
+///////////////////////////////
+exports.getWomensClothing = async (req, res) => {
+  console.log(req.body);
+  const femaleClothing = await Item.findAll({
+    where: { category: "Women's Clothing" },
+  });
+  res.render('category', { femaleClothing });
+};
