e73d40256e2ec98f5ac471b2921c2ab2180ec6d2
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2021-12-07 13:58:49 -0700

    Update category.js

--- controllers/category.js
+++ controllers/category.js
@@ -4,36 +4,36 @@ const { User, Item, Cart } = require('../index');
 //      JEWELRY CATEGORY      //
 ///////////////////////////////
 exports.getJewelry = async (req, res) => {
-  const jewelry = await Item.findAll({ where: { category: 'jewelry' } });
-  res.render('category', { jewelry });
+  const category = await Item.findAll({ where: { category: 'Jewelry' } });
+  res.render('category', { category });
 };
 
 ////////////////////////////////
 //      ELECTRONICS CATEGORY      //
 ///////////////////////////////
 exports.getElectronics = async (req, res) => {
-  const electronics = await Item.findAll({
+  const category = await Item.findAll({
     where: { category: 'Electronics' },
   });
-  res.render('category', { electronics });
+  res.render('category', { category });
 };
 
 ////////////////////////////////
 //  MEN'S CLOTHING CATEGORY   //
 ///////////////////////////////
 exports.getMensClothing = async (req, res) => {
-  const maleClothing = await Item.findAll({
+  const category = await Item.findAll({
     where: { category: "Men's Clothing" },
   });
-  res.render('category', { maleClothing });
+  res.render('category', { category });
 };
 
 ////////////////////////////////
 //      WOMEN'S CATEGORY      //
 ///////////////////////////////
 exports.getWomensClothing = async (req, res) => {
-  const femaleClothing = await Item.findAll({
+  const category = await Item.findAll({
     where: { category: "Women's Clothing" },
   });
-  res.render('category', { femaleClothing });
+  res.render('category', { category });
 };
