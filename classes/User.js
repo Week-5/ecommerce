6f2765fde636abfd4482190a1bf7b5fd6a9220ab
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2021-12-07 13:58:37 -0700

    Update User.js

--- classes/User.js
+++ classes/User.js
@@ -1,26 +1,26 @@
-const { db, DataTypes, Model } = require('../db');
+const {db, DataTypes, Model} = require('../db');
 /**
  * User class extends Model
  */
 class User extends Model {}
 
 User.init(
-	{
-		username: {
-			type: DataTypes.STRING,
-			primaryKey: true,
-		},
-		fullName: DataTypes.STRING,
-		email: DataTypes.STRING,
-		password: DataTypes.STRING,
-		isAdmin: DataTypes.BOOLEAN,
-	},
-	{
-		sequelize: db,
-		timestamps: false,
-	}
+    {
+      username: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+      },
+      fullName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      isAdmin: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize: db,
+      timestamps: false,
+    },
 );
 
 module.exports = {
-	User,
+  User,
 };
