5ee8cce7e1c1105f1b8761ab7a3a9531b797a803
Eric Davenport <edavenportjr@google.com>
2021-12-07 16:45:27 +0000

    user test passing - item incomplete - still accessing cart-commit to update cart CRUD

--- test/Item.test.js
+++ test/Item.test.js
@@ -1,3 +1,4 @@
+const expectExport = require('expect');
 const { User, Item, Cart } = require('../index')
 
 describe('Item Object Test', () => {
@@ -5,34 +6,34 @@ describe('Item Object Test', () => {
     // Need to be able to add items to cart -> what is the way it is being added
     //      will it be via cart.addItem(item)  ||  user.items.push(item)
 
-    // test('Item can be added to cart', async () => {
-    //     const newUser2 = await User.create({ 
-    //         username: 'yourFavCustomer', 
-    //         fullName: 'Cust Test', 
-    //         email: 'newCustn@gmail.com', 
-    //         password: 'password', 
-    //         isAdmin: false,
-    //         cart: {
-    //             totalPrice: 0
-    //         } 
-    //     }), {
-    //         include: [ Cart ]
-    //     };
+    test('Item can be added to cart', async () => {
+        const newUser2 = await User.create({ 
+            username: 'yourFavCustomer', 
+            fullName: 'Cust Test', 
+            email: 'newCustn@gmail.com', 
+            password: 'password', 
+            isAdmin: false,
+            
+        })
 
-    //     const macBook = await Item.create({ 
-    //         title:'Macbook', 
-    //         stock: 4, 
-    //         price: 1230.90, 
-    //         description: 'Brand New Used', 
-    //         category: 'Electronics', 
-    //         image: '', 
-    //         clickCount: 0 
-    //     });
+        const macBook = await Item.create({ 
+            title:'Macbook', 
+            stock: 4, 
+            price: 1230.90, 
+            description: 'Brand New Used', 
+            category: 'Electronics', 
+            image: '', 
+            clickCount: 0 
+        });
+
+        const cart = newUser2.Cart
+        cart.addItem(macBook)
+        console.log(cart)
+        expectExport(cart.length).toBe(1)
 
 
 
-
-    // });
+    });
 
 
 });
--- test/User.test.js
+++ test/User.test.js
@@ -1,27 +1,76 @@
-const { User } = require('../index')
+const { User, Item } = require('../index')
 const { seed } = require('../seed')
 const { db } = require('../db')
 
 describe('User Object Test', () => {
     // seed();
     beforeAll(async () => {
-        await db.sync({ force: true})
+        await db.sync({ force: true});
     });
     
-    test('User can  be created', async () => {
-        const newUser = await User.create({ username: 'WhomIsThis', fullName: 'Mike J', email: 'WhoMJ@gmail.com', password: 'password', isAdmin: true})
-        const newUser1 = await User.create({ username: 'Whs2', fullName: 'Mik0989', email: 'WhsMJ@gmai==l.com', password: 'password', isAdmin: true})
-        const newUser2 = await User.create({ username: 'WhomI000is', fullName: 'M J', email: 'WMJ@gmail.com', password: 'password', isAdmin: false})
+    test('User can be created', async () => {
+        const newUser = await User.create({ username: 'WhomIsThis', fullName: 'Mike J', email: 'WhoMJ@gmail.com', password: 'password', isAdmin: true});
+        
+        const users = await User.findAll();
 
-        const users = await User.findAll()
-
-        expect(users.length).toBe(3)
+        expect(users[0].username).toBe(newUser.username);
     });
 
-    test('user can be an admin', async () => {
-        const newUser = await User.create({ username: 'yourFavAdmin', fullName: 'Admin Test', email: 'newAdmin@gmail.com', password: 'password', isAdmin: true})
-        expect(newUser.isAdmin).toBeTruthy()
+    test('User can be an admin', async () => {
+        const newUser = await User.create({ username: 'yourFavAdmin', fullName: 'Admin Test', email: 'newAdmin@gmail.com', password: 'password', isAdmin: true});
+        expect(newUser.isAdmin).toBeTruthy();
     });
+
+    // This test if a user can create and add an item to the inventory as an admin
+    test('User can create new item', async () => {
+        // This test should normally test the function for adding an item
+        // Like call where the func would be located and the test the results
+        // now i have to create a user - then item then test if the item belongs to the user
+
+        // there should be a guard that the user is an admin
+        // creates new user
+        let user = await User.create({
+            username: 'little E', 
+            fullName: 'Erika D', 
+            email: 'ED234@gnail.com', 
+            passwod: 'password', 
+            isAdmin: true});
+
+        // guard against unfilled fields on front end
+        // creates new item
+        let item = await Item.create({
+            title: 'New Bush',
+            stock: 4,
+            price: 3.0,
+            description: 'New type of bush for your front lawn',
+            category: 'Electronics',
+            image: '',
+            clickCount: 3,
+            CartId: null,
+            UserUsername: 'little E'
+        });
+
+        let item2 = await Item.create({
+            title: 'New Bush22',
+            stock: 42,
+            price: 32.0,
+            description: 'New type of bush for your front lawn',
+            category: 'Electronics',
+            image: 'dfgasdfsgfd.jpg',
+            clickCount: 3,
+            CartId: null,
+            UserUsername: 'little E'
+        });
+        // sequelize helper function to add the association between the item created and user
+        await user.addItem(item);
+        await user.addItem(item2);
+        // loaf all of the users current 
+        let userItems = await user.getItems()
+       
+        expect(userItems[0].UserUsername).toBe(item.UserUsername)
+    });
+
+
 
     
 });
