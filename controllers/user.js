370bef1348c1a2217a0b2b1348e05566570d07c4
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2021-12-08 14:37:06 -0700

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 const { validationResult } = require('express-validator');
 const { noExtendLeft } = require('sequelize/dist/lib/operators');
 const { User, Item, Cart } = require('../index');
@@ -13,39 +13,38 @@ exports.getCreateUser = async (req, res) => {
 // create a new user
 exports.postCreateUser = async (req, res, next) => {
   try {
-    const errors = validationResult(req)
-    console.log(errors)
+    const errors = validationResult(req);
+    console.log(errors);
     if (!errors.isEmpty()) {
-      res.status(422).json( { errors: errors.array() })
+      res.status(422).json({ errors: errors.array() });
     }
 
     const newUsername = req.body.username;
     const newFullName = req.body.fullName;
     const newEmail = req.body.email;
     const newPassword = req.body.password;
-  
-    const hashedPassword = await bcrypt.hash(newPassword, 12)
-  
+
+    const hashedPassword = await bcrypt.hash(newPassword, 12);
+
     const newUser = await User.create({
       username: newUsername,
       fullName: newFullName,
       email: newEmail,
       password: hashedPassword,
-      isAdmin: 0
+      isAdmin: 0,
     });
-  
+
     // create new cart and assign it to new user
     const newUserCart = await Cart.create({
       totalPrice: 0,
       UserUsername: newUser.username,
     });
-    req.flash('success', 'Successfully created account')
+    req.flash('success', 'Successfully created account');
     res.status(200).redirect(301, `/homepage/${newUser.username}`);
-
-  } catch(err) {
-    const error = new Error(err)
-    error.httpStatusCode = 500
-    return next(error)
+  } catch (err) {
+    const error = new Error(err);
+    error.httpStatusCode = 500;
+    return next(error);
   }
 };
 
@@ -61,20 +60,17 @@ exports.getLogIn = async (req, res) => {
 exports.postLogIn = async (req, res) => {
   const inputName = req.body.username;
   const inputPassword = req.body.password;
-  const checkUser = await User.findByPk(inputName);
-  let loggedUsername = false;
-  let loggedPassword = false;
-  if (checkUser) {
+  const user = await User.findByPk(inputName);
+  if (user) {
     loggedUsername = true;
   }
-  if (inputPassword === checkUser.password) {
-    loggedPassword = true;
-  }
 
-  if (loggedUsername && loggedPassword) {
+  const match = await bcrypt.compare(inputPassword, user.password);
+
+  if (match) {
     res.status(200).redirect(301, `/homepage/${inputName}`);
   } else {
-    res.status(200).redirect(301, '/user/create-account');
+    res.status(200).redirect(301, '/create-account');
   }
 };
 
@@ -92,7 +88,7 @@ exports.getUser = async (req, res) => {
     items: items,
     adminItems: adminItems,
   };
-  res.status(200).render('user', { data });
+  res.render('user', { data });
 };
 
 ////////////////////////////////
@@ -101,13 +97,7 @@ exports.getUser = async (req, res) => {
 // render user update form
 exports.getUpdateUser = async (req, res) => {
   const user = await User.findByPk(req.params.username);
-  const cart = await Cart.findOne({ where: { UserUsername: user.username } });
-  const items = await cart.getItems();
-  const data = {
-    user: user,
-    items: items,
-  };
-  res.status(200).render('userUpdate', { data });
+  res.status(200).render('userUpdate', { user });
 };
 // user update
 exports.postUpdateUser = async (req, res) => {
@@ -115,17 +105,16 @@ exports.postUpdateUser = async (req, res) => {
   const updatedFullName = req.body.fullName;
   const updatedEmail = req.body.email;
   const updatedPassword = req.body.password;
-  const updatedIsAdmin = req.body.isAdmin;
+  const updatedIsAdmin = req.body.isAdmin === 'on' ? 1 : 0;
 
   user.set({
     fullName: updatedFullName === '' ? user.fullName : updatedFullName,
     email: updatedEmail === '' ? user.email : updatedEmail,
     password: updatedPassword === '' ? user.password : updatedPassword,
-    isAdmin: updatedIsAdmin === undefined ? user.isAdmin : updatedIsAdmin,
+    isAdmin: updatedIsAdmin === '' ? user.isAdmin : updatedIsAdmin,
   });
 
   await user.save();
-
   res.status(200).redirect(301, `/users/${user.username}`);
 };
 
@@ -136,5 +125,5 @@ exports.postUpdateUser = async (req, res) => {
 exports.deleteUser = async (req, res) => {
   const user = await User.findByPk(req.params.username);
   await user.destroy();
-  res.status(200).redirect(301, '/homepage');
+  res.redirect(301, '/homepage');
 };
