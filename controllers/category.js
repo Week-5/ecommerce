5e86bc8ee307b5eba997dc62ccaaa6c916fac2c9
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2021-12-08 11:42:55 -0700

    fixed some dry codes

--- controllers/category.js
+++ controllers/category.js
@@ -4,36 +4,102 @@ const { User, Item, Cart } = require('../index');
 //      JEWELRY CATEGORY      //
 ///////////////////////////////
 exports.getJewelry = async (req, res) => {
-  const category = await Item.findAll({ where: { category: 'Jewelry' } });
-  res.render('category', { category });
+  const user = await User.findByPk(req.params.username);
+  const category = await Item.findAll({ where: { category: "Jewelry" } });
+
+  let data = {
+    category: category,
+  };
+
+  if (user !== null) {
+    const cart = await Cart.findOne({ where: { UserUsername: user.username } });
+    const items = await cart.getItems();
+
+    data = {
+      user: user,
+      items: items,
+      category: category,
+    };
+  }
+
+  res.render('category', { data });
 };
 
 ////////////////////////////////
 //      ELECTRONICS CATEGORY      //
 ///////////////////////////////
 exports.getElectronics = async (req, res) => {
-  const category = await Item.findAll({
-    where: { category: 'Electronics' },
-  });
-  res.render('category', { category });
+  const user = await User.findByPk(req.params.username);
+  const category = await Item.findAll({ where: { category: "Electronics" } });
+
+  let data = {
+    category: category,
+  };
+
+  if (user !== null) {
+    const cart = await Cart.findOne({ where: { UserUsername: user.username } });
+    const items = await cart.getItems();
+
+    data = {
+      user: user,
+      items: items,
+      category: category,
+    };
+  }
+
+  res.render('category', { data });
 };
 
 ////////////////////////////////
 //  MEN'S CLOTHING CATEGORY   //
 ///////////////////////////////
 exports.getMensClothing = async (req, res) => {
+  const user = await User.findByPk(req.params.username);
   const category = await Item.findAll({
     where: { category: "Men's Clothing" },
   });
-  res.render('category', { category });
+
+  let data = {
+    category: category,
+  };
+
+  if (user !== null) {
+    const cart = await Cart.findOne({ where: { UserUsername: user.username } });
+    const items = await cart.getItems();
+
+    data = {
+      user: user,
+      items: items,
+      category: category,
+    };
+  }
+
+  res.render('category', { data });
 };
 
 ////////////////////////////////
 //      WOMEN'S CATEGORY      //
 ///////////////////////////////
 exports.getWomensClothing = async (req, res) => {
+  const user = await User.findByPk(req.params.username);
   const category = await Item.findAll({
     where: { category: "Women's Clothing" },
   });
-  res.render('category', { category });
+
+  let data = {
+    category: category,
+  };
+
+  if (user !== null) {
+    const cart = await Cart.findOne({ where: { UserUsername: user.username } });
+    const items = await cart.getItems();
+
+    data = {
+      user: user,
+      items: items,
+      category: category,
+    };
+  }
+
+  res.render('category', { data });
 };
