9e2697fd6f580c35e3e879da33b2193c1e4ad2c1
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2021-12-08 11:49:41 -0700

    fixed dry codes

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -1,39 +1,34 @@
 const { User, Item, Cart } = require('../index');
 
 ////////////////////////////////
-//        HOMEPAGE GUEST      //
+//           HOMEPAGE         //
 ///////////////////////////////
-exports.getHomepageGuest = async (req, res) => {
-  const allItems = await Item.findAll();
-  const popularItems = allItems.slice(0, 4);
-  const data = {
-    allItems: allItems,
-    popularItems: popularItems,
-  };
-  res.status(200).render('homepage', { data });
-};
 
-////////////////////////////////
-//        HOMEPAGE USER      //
-///////////////////////////////
-exports.getHomepageUser = async (req, res) => {
+exports.getHomepage = async (req, res) => {
   const user = await User.findByPk(req.params.username);
-  const cart = await Cart.findOne({
-    where: { UserUsername: user.username },
-  });
-  const items = await user.getItems();
   const allItems = await Item.findAll();
-  const popularItems = allItems.slice(0, 4);
-  // allItems.sort(function(a, b) {
-  //   return a-b;
-  // });
-  const data = {
-    user: user,
-    userItems: items,
-    userCart: cart,
+  const popularItems = allItems.slice(-4);
+
+  let data = {
     allItems: allItems,
     popularItems: popularItems,
   };
+
+  if (user !== null) {
+    const cart = await Cart.findOne({
+      where: { UserUsername: user.username },
+    });
+
+    const items = await user.getItems();
+    
+    data = {
+      user: user,
+      userItems: items,
+      userCart: cart,
+      allItems: allItems,
+      popularItems: popularItems,
+    };
+  }
 
   res.status(200).render('homepage', { data });
 };
