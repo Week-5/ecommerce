bc9eda06e6e16cdb27c0215de219f622822db3b4
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2021-12-08 14:50:59 -0700

    Update server.js
    fixed imports

--- server.js
+++ server.js
@@ -8,10 +8,6 @@ const flash = require('connect-flash')
 const express = require('express');
 const app = express();
 
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const flash = require('connect-flash');
-
 // seed database
 const seed = require('./seed');
 seed();
