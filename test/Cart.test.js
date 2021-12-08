917a6ec1ecf2ecc9f6656007dd09302b7b058953
Eric Davenport <edavenportjr@google.com>
2021-12-08 16:32:09 +0000

    cart started

--- test/Cart.test.js
+++ test/Cart.test.js
@@ -3,4 +3,12 @@ const { Cart } = require('../index')
 describe('Cart Object Test', () => {
     // TODO: No Cart test created yet
     // Once cart functionality is address -> will be able to maneuver the cart object
+    // cart belongs to user
+    // Cart functionality 
+    // Add items 
+    // Remove single item
+    // Total price is correct
+    // Remove all items
+
+
 });
